caf35448e676569c30ce751f7d8fb265a20528df
Mitchell Mudd <developer@mitchellmudd.dev>
2024-08-12 08:21:47 -0400

    Learned how generics worked and finished the lessons (other than the API one I'll put in a different repo)

--- generics.go
+++ generics.go
@@ -1,5 +1,62 @@
 package main
 
-func generics() {
+import "fmt"
 
+type gasEngine2 struct {
+	gallons float32
+	mpg     float32
+}
+
+type electricEngine2 struct {
+	kwh   float32
+	mpkwh float32
+}
+
+type car[T gasEngine2 | electricEngine2] struct {
+	carMake  string
+	carModel string
+	engine   T
+}
+
+func generics() {
+	var intSlice = []int{1, 2, 3, 4, 5}
+	fmt.Println(sumSlice[int](intSlice))
+
+	var float32Slice = []float32{1.0, 2.0, 3.0, 4.0, 5.0}
+	fmt.Println(sumSlice[float32](float32Slice))
+
+	var float64Slice = []float64{1.0, 2.0, 3.0, 4.0, 5.0}
+	fmt.Println(sumSlice[float64](float64Slice))
+
+	var gasCar = car[gasEngine2]{
+		carMake:  "Honda",
+		carModel: "Accord",
+		engine: gasEngine2{
+			gallons: 1.0,
+			mpg:     2.0,
+		},
+	}
+
+	fmt.Println(gasCar)
+
+	var electricCar = car[electricEngine2]{
+		carMake:  "Honda",
+		carModel: "Accord E",
+		engine: electricEngine2{
+			kwh:   1.0,
+			mpkwh: 2.0,
+		},
+	}
+
+	fmt.Println(electricCar)
+
+	return
+}
+
+func sumSlice[T int | float32 | float64](slice []T) T {
+	var sum T
+	for _, v := range slice {
+		sum += v
+	}
+	return sum
 }
--- main.go
+++ main.go
@@ -14,4 +14,5 @@ func main() {
 	goRoutines()
 	channels()
 	generics()
+	return
 }
